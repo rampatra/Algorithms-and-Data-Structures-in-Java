5cfb4898ea2e62a67b6f3e271207dccfec0d9976
Ram swaroop <ramswaroop@bsb.in>
2015-04-22 20:15:49 +0530

    tree mirror done

--- src/me/ramswaroop/trees/BinaryTree.java
+++ src/me/ramswaroop/trees/BinaryTree.java
@@ -23,15 +23,45 @@ public class BinaryTree<E> extends Tree<E> {
         }
     }
 
+    public boolean isIdentical(BinaryNode<E> node) {
+        return isIdentical(this.root, node);
+    }
+
+    /**
+     * Converts a Tree to its Mirror Tree.
+     * <p/>
+     * MIRROR OF A BINARY TREE T is another Binary Tree M(T) with
+     * left and right children of all non-leaf nodes interchanged.
+     * <p/>
+     * TIP: In-order traversal of mirror tree is exactly the
+     * reverse of the in-order traversal of the original tree.
+     */
+    public void mirror() {
+        mirror(root);
+    }
+
+    public void mirror(BinaryNode<E> node) {
+        if (node == null) return;
+
+        BinaryNode<E> tempNode;
+
+        // mirror sub-trees
+        mirror(node.left);
+        mirror(node.right);
+
+        // swap nodes
+        tempNode = node.left;
+        node.left = node.right;
+        node.right = tempNode;
+    }
+
     public int height() {
         return height(root);
     }
 
     public int height(BinaryNode<E> node) {
-        return 0;
-    }
+        if (node == null) return 0;
 
-    public boolean isIdentical(BinaryNode<E> node) {
-        return isIdentical(this.root, node);
+        return Math.max(height(node.left), height(node.right)) + 1;
     }
 }
