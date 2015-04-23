f0409746357c559f8e7ec27b820a7be92d44b0e0
Ram swaroop <ramswaroop@bsb.in>
2015-04-23 09:44:10 +0530

    tree root to leaf done

--- src/me/ramswaroop/trees/BinaryTree.java
+++ src/me/ramswaroop/trees/BinaryTree.java
@@ -1,6 +1,7 @@
 package me.ramswaroop.trees;
 
 import me.ramswaroop.common.BinaryNode;
+import me.ramswaroop.utils.Utils;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,8 +11,18 @@ import me.ramswaroop.common.BinaryNode;
  * To change this template go to Preferences | IDE Settings | File and Code Templates
  */
 public class BinaryTree<E> extends Tree<E> {
+
     BinaryNode<E> root;
 
+    /**
+     * Checks whether two trees having their roots at node1 and node2
+     * are identical or not.
+     *
+     * @param node1
+     * @param node2
+     * @param <E>
+     * @return
+     */
     public static <E> boolean isIdentical(BinaryNode<E> node1, BinaryNode<E> node2) {
         if (node1 == null && node2 == null) return true;
         if (node1 == null && node2 != null || (node1 != null && node2 == null)) return false;
@@ -21,6 +32,83 @@ public class BinaryTree<E> extends Tree<E> {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Prints the pre-order traversal of the tree.
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    public void preOrder(BinaryNode<E> node) {
+        if (node == null) {
+            return;
+        }
+        Utils.print(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * Prints the in-order traversal of the tree.
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    public void inOrder(BinaryNode<E> node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        Utils.print(node.value);
+        inOrder(node.right);
+    }
+
+    /**
+     * Prints the post-order traversal of the tree.
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    public void postOrder(BinaryNode<E> node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        Utils.print(node.value);
+    }
+
+    /**
+     * Deletes a particular node from the tree.
+     *
+     * @param value
+     */
+    public void delete(E value) {
+
+    }
+
+    /**
+     * TODO
+     * Deletes the entire tree.
+     */
+    public void delete() {
+        delete(root);
+        root = null;
+    }
+
+    public void delete(BinaryNode<E> node) {
+        if (node == null) {
+            return;
+        }
+        // first delete the child nodes
+        delete(node.left);
+        delete(node.right);
+        //Utils.println("Deleting node: " + node.value);
+        node = null; // delete node
     }
 
     public boolean isIdentical(BinaryNode<E> node) {
@@ -55,6 +143,11 @@ public class BinaryTree<E> extends Tree<E> {
         node.right = tempNode;
     }
 
+    /**
+     * Return the height of the tree.
+     *
+     * @return
+     */
     public int height() {
         return height(root);
     }
@@ -63,5 +156,60 @@ public class BinaryTree<E> extends Tree<E> {
         if (node == null) return 0;
 
         return Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    /**
+     * Returns the number of nodes currently in the tree.
+     *
+     * @return
+     */
+    public int size() {
+        return size(root);
+    }
+
+    public int size(BinaryNode<E> node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return size(node.left) + 1 + size(node.right);
+        }
+    }
+
+    /**
+     * Tests if this tree is empty.
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Prints the node to leaf paths, one per line.
+     */
+    public void rootToLeafPaths() {
+        E[] pathList = (E[]) new Object[100];
+        rootToLeafPaths(root, pathList, 0);
+    }
+
+    public void rootToLeafPaths(BinaryNode<E> node, E[] pathList, int pathLength) {
+        if (node == null) return;
+
+        pathList[pathLength] = node.value;
+        pathLength++;
+
+        // if its a leaf node then print the list
+        if (node.left == null && node.right == null) {
+            int i;
+            for (i = 0; i < pathLength - 1; i++) {
+                Utils.print(pathList[i] + " -> ");
+            }
+            // outside the loop so that "->" doesn't appear after the last node
+            Utils.println(pathList[i]);
+        } else {
+            // do the same for subtrees
+            rootToLeafPaths(node.left, pathList, pathLength);
+            rootToLeafPaths(node.right, pathList, pathLength);
+        }
     }
 }
--- src/me/ramswaroop/trees/RecursiveBST.java
+++ src/me/ramswaroop/trees/RecursiveBST.java
@@ -12,26 +12,37 @@ import me.ramswaroop.utils.Utils;
  */
 public class RecursiveBST<E extends Comparable<E>> extends BinarySearchTree<E> {
 
-    //BinaryNode<E> root;
-
     public static void main(String[] a) {
         RecursiveBST obj = new RecursiveBST();
         obj.put(6);
         obj.put(3);
         obj.put(5);
         obj.put(7);
+        obj.put(8);
+        obj.put(9);
         obj.preOrder();
-        obj.print("\n");
+        Utils.println("");
         obj.inOrder();
-        obj.print("\n");
+        Utils.println("");
         obj.postOrder();
         Utils.println("\n" + obj.size());
         Utils.println(BinaryTree.isIdentical(obj.root.right, obj.root.right));
         Utils.println(obj.isIdentical(obj.root));
+        Utils.println(obj.height());
+        /*obj.delete();
+        Utils.println("After deletion: ");
+        obj.postOrder();*/
+        Utils.println("In Order: ");
+        obj.inOrder();
+        Utils.println("\nAfter mirroring: ");
+        obj.mirror();
+        obj.inOrder();
+        Utils.println("\nRoot to leafs: ");
+        obj.rootToLeafPaths();
     }
 
     /**
-     * Inserts a node to the tree.
+     * Inserts a node into the tree.
      *
      * @param value
      */
@@ -58,97 +69,4 @@ public class RecursiveBST<E extends Comparable<E>> extends BinarySearchTree<E> {
             }
         }
     }
-
-    /**
-     * Deletes a particular node from the tree.
-     *
-     * @param value
-     */
-    public void delete(E value) {
-
-    }
-
-    /**
-     * Prints the pre-order traversal of the tree.
-     */
-    public void preOrder() {
-        preOrder(root);
-    }
-
-    public void preOrder(BinaryNode<E> node) {
-        if (node == null) {
-            return;
-        }
-        print(node.value);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-
-    /**
-     * Prints the in-order traversal of the tree.
-     */
-    public void inOrder() {
-        inOrder(root);
-    }
-
-    public void inOrder(BinaryNode<E> node) {
-        if (node == null) {
-            return;
-        }
-        inOrder(node.left);
-        print(node.value);
-        inOrder(node.right);
-    }
-
-    /**
-     * Prints the post-order traversal of the tree.
-     */
-    public void postOrder() {
-        postOrder(root);
-    }
-
-    public void postOrder(BinaryNode<E> node) {
-        if (node == null) {
-            return;
-        }
-        postOrder(node.left);
-        postOrder(node.right);
-        print(node.value);
-    }
-
-    /**
-     * Returns the number of nodes currently in the tree.
-     *
-     * @return
-     */
-    public int size() {
-        return size(root);
-    }
-
-    public int size(BinaryNode<E> node) {
-        if (node == null) {
-            return 0;
-        } else {
-            return size(node.left) + 1 + size(node.right);
-        }
-    }
-
-    /**
-     * Tests if this tree is empty.
-     *
-     * @return
-     */
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    /**
-     * Utility methods
-     *
-     * @param value
-     */
-    private void print(E value) {
-        System.out.print(value);
-    }
-
 }
