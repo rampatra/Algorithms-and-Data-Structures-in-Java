7622c67576dac0bfdc40f777946e7322f38f21ae
Ram swaroop <ramswaroop@bsb.in>
2015-05-10 16:12:22 +0530

    tree: spiral traversal done using stacks

--- src/me/ramswaroop/trees/BinaryTree.java
+++ src/me/ramswaroop/trees/BinaryTree.java
@@ -1,11 +1,10 @@
 package me.ramswaroop.trees;
 
-import me.ramswaroop.common.BinaryNode;
-import me.ramswaroop.common.LinkedQueue;
-import me.ramswaroop.common.Queue;
+import me.ramswaroop.common.*;
 import me.ramswaroop.utils.Utils;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -30,10 +29,13 @@ public class BinaryTree<E extends Comparable<E>> extends Tree<E> {
         binaryTree.put(4);
         binaryTree.put(5);
         //binaryTree.put(16);
+        Utils.print("Breadth-first Traversal: ");
         binaryTree.breadthFirstTraversal();
-        Utils.println("");
+        Utils.print("\nSpiral Traversal: ");
+        binaryTree.spiralTraversal();
+        Utils.print("\nIn order traversal: ");
         binaryTree.inOrder();
-        Utils.println("\nIs BST: " + binaryTree.isBST());
+        Utils.print("\nIs BST: " + binaryTree.isBST());
     }
 
     /**
@@ -166,6 +168,90 @@ public class BinaryTree<E extends Comparable<E>> extends Tree<E> {
             breadthFirstTraversalUsingQueue(queue.remove(), queue);
         } catch (NoSuchElementException e) {
             return;
+        }
+    }
+
+
+    /**
+     * In spiral order traversal, nodes at different levels
+     * are printed in alternating order.
+     */
+    public void spiralTraversal() {
+        //spiralTraversal(root, 0); // uses recursion
+        spiralTraversal(root); // uses 2 stacks
+    }
+
+    public void spiralTraversal(BinaryNode<E> node, int level) {
+        if (node == null) return;
+
+        // print the starting node
+        if (level == 0) printValue(node);
+
+        // print the neighbour nodes
+        if (level % 2 == 0) {
+            printValue(node.left);
+            printValue(node.right);
+        } else {
+            printValue(node.right);
+            printValue(node.left);
+        }
+
+        // go to next level
+        level++;
+        if (level % 2 == 0) {
+            spiralTraversal(node.left, level);
+            spiralTraversal(node.right, level);
+        } else {
+            spiralTraversal(node.right, level);
+            spiralTraversal(node.left, level);
+        }
+    }
+
+    public void spiralTraversal(BinaryNode<E> node) {
+        Stack<BinaryNode<E>> stack1 = new LinkedStack<>(); // for nodes to be printed ltr
+        Stack<BinaryNode<E>> stack2 = new LinkedStack<>(); // for nodes to be printed rtl
+
+        printValue(node);
+
+        stack1.push(node.right);
+        stack1.push(node.left);
+
+        // pop stack1 and push their child nodes in stack2
+        while (!stack1.isEmpty()) {
+
+            BinaryNode<E> leftChild = stack1.pop();
+            BinaryNode<E> rightChild = stack1.pop();
+
+            printValue(leftChild);
+            printValue(rightChild);
+
+            try {
+                if (leftChild != null) stack2.push(leftChild.left);
+                if (leftChild != null) stack2.push(leftChild.right);
+                if (rightChild != null) stack2.push(rightChild.left);
+                if (rightChild != null) stack2.push(rightChild.right);
+            } catch (EmptyStackException e) {
+                // ignore error when stack empty
+            }
+        }
+
+        // pop stack2 and push their child nodes in stack1
+        while (!stack2.isEmpty()) {
+
+            BinaryNode<E> rightChild = stack2.pop();
+            BinaryNode<E> leftChild = stack2.pop();
+
+            printValue(rightChild);
+            printValue(leftChild);
+
+            try {
+                if (rightChild != null) stack1.push(rightChild.right);
+                if (rightChild != null) stack1.push(rightChild.left);
+                if (leftChild != null) stack1.push(leftChild.right);
+                if (leftChild != null) stack1.push(leftChild.left);
+            } catch (EmptyStackException e) {
+                // ignore error when stack empty
+            }
         }
     }
 
@@ -422,6 +508,8 @@ public class BinaryTree<E extends Comparable<E>> extends Tree<E> {
 
     /**
      * Traverse the tree in in-order fashion and keep track of prev node.
+     * <p/>
+     * TODO Clarify doubt: Why it doesn't work if I replace "prev.value" with "prev"
      *
      * @param node
      * @param prev
