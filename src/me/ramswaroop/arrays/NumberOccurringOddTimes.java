123f50c58e278fe1ea2944bf127ff01c15aa60d1
Ram swaroop <ramswaroop@bsb.in>
2015-05-20 23:37:43 +0530

    arrays: number occurring odd no of times

---
+++ src/me/ramswaroop/arrays/NumberOccurringOddTimes.java
@@ -0,0 +1,29 @@
+package me.ramswaroop.arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 5/20/15
+ * @time: 11:09 PM
+ */
+
+/**
+ * Given an array of positive integers. All numbers occur
+ * even number of times except one number which occurs odd
+ * number of times. Find the number in O(n) time & constant space.
+ */
+public class NumberOccurringOddTimes {
+
+    public static int numberOccurringOddTimes(int a[]) {
+        int res = a[0];
+        for (int i = 1; i < a.length; i++) {
+            res ^= a[i];
+        }
+        return res;
+    }
+
+    public static void main(String a[]) {
+        System.out.print(numberOccurringOddTimes(new int[]{2, 3, 3, 3, 1, 2, 1}));
+    }
+}
