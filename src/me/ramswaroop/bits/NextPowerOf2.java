8f027c4164cffd25e6894e9c58efbb2edf164dfb
Ram swaroop <ramswaroop@bsb.in>
2015-06-04 15:14:41 +0530

    next power of 2 done (in 3 ways)

--- src/me/ramswaroop/bits/NextPowerOf2.java
+++ src/me/ramswaroop/bits/NextPowerOf2.java
@@ -9,30 +9,72 @@ package me.ramswaroop.bits;
  */
 public class NextPowerOf2 {
 
+
     /**
-     * Finds the left most set bit position and returns
-     * 2 to the power of that position plus 1.
+     * Left shifts 1 number of times equal to the
+     * leftmost set bit position in n.
      *
      * @param n
      * @return
      */
     public static long nextPowerOf2(long n) {
+        // if n is already a power of 2 then return n
+        if (n != 0 && ((n & (n - 1)) == 0)) return n;
+
+        long p = 1;
+        while (p < n) {
+            p <<= 1;
+        }
+        return p;
+    }
+
+    /**
+     * Finds the leftmost set bit position and
+     * left shifts 1 that many times.
+     *
+     * @param n
+     * @return
+     */
+    public static long nextPowerOf2_V2(long n) {
+        // if n is already a power of 2 then return n
+        if (n != 0 && ((n & (n - 1)) == 0)) return n;
+
+        long c = 0;
+        // finds the position of the leftmost set bit
+        while (n > 0) {
+            n >>= 1;
+            c++;
+        }
+
+        return 1 << c;
+    }
+
+    /**
+     * Finds the leftmost set bit position and
+     * left shifts 1 that many times.
+     *
+     * @param n
+     * @return
+     */
+    public static long nextPowerOf2_V1(long n) {
         if (PowerOf2.isPowerOf2(n)) {
             return n;
         }
 
         int c = 0;
-        for (int i = 0; i < 64; i++) {
+        // finds the position of leftmost set bit
+        for (int i = 1; i <= 64; i++) {
             if ((n & 1) == 1) {
                 c = i;
             }
             n >>= 1;
         }
 
-        return (c == 0) ? 1 : (long) Math.pow(2, (c + 1)); // check added for input 0
+        return 1 << c;
     }
 
     public static void main(String a[]) {
+
         System.out.println(nextPowerOf2(2));
         System.out.println(nextPowerOf2(3));
         System.out.println(nextPowerOf2(18));
@@ -40,5 +82,25 @@ public class NextPowerOf2 {
         System.out.println(nextPowerOf2(7));
         System.out.println(nextPowerOf2(1));
         System.out.println(nextPowerOf2(0));
+
+        System.out.println("=================");
+
+        System.out.println(nextPowerOf2_V2(2));
+        System.out.println(nextPowerOf2_V2(3));
+        System.out.println(nextPowerOf2_V2(18));
+        System.out.println(nextPowerOf2_V2(6));
+        System.out.println(nextPowerOf2_V2(7));
+        System.out.println(nextPowerOf2_V2(1));
+        System.out.println(nextPowerOf2_V2(0));
+
+        System.out.println("=================");
+
+        System.out.println(nextPowerOf2_V1(2));
+        System.out.println(nextPowerOf2_V1(3));
+        System.out.println(nextPowerOf2_V1(18));
+        System.out.println(nextPowerOf2_V1(6));
+        System.out.println(nextPowerOf2_V1(7));
+        System.out.println(nextPowerOf2_V1(1));
+        System.out.println(nextPowerOf2_V1(0));
     }
 }
