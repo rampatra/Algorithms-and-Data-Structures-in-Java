39aa7a3e54b0d51d8e59db2e719d4e6835d8124a
Ram swaroop <ramswaroop@bsb.in>
2015-06-04 15:56:16 +0530

    rightmost set bit done

---
+++ src/me/ramswaroop/bits/RightmostSetBit.java
@@ -0,0 +1,32 @@
+package me.ramswaroop.bits;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 6/3/15
+ * @time: 11:56 PM
+ */
+public class RightmostSetBit {
+
+    public static int getRightmostSetBitPosition(long n) {
+        int position = 0;
+        while (n > 0) {
+            position++;
+            if ((n & 1) == 1) {
+                break;
+            }
+            n >>= 1;
+        }
+        return position;
+    }
+
+    public static void main(String a[]) {
+        System.out.println(getRightmostSetBitPosition(0));
+        System.out.println(getRightmostSetBitPosition(1));
+        System.out.println(getRightmostSetBitPosition(2));
+        System.out.println(getRightmostSetBitPosition(5));
+        System.out.println(getRightmostSetBitPosition(18));
+        System.out.println(getRightmostSetBitPosition(19));
+    }
+}
