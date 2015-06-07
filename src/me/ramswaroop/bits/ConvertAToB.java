1c28b442eea5e680969f101b4f8fa20b372419a7
Ram swaroop <ramswaroop@bsb.in>
2015-06-07 13:57:51 +0530

    bit required to change to convert a to b: done

---
+++ src/me/ramswaroop/bits/ConvertAToB.java
@@ -0,0 +1,21 @@
+package me.ramswaroop.bits;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 6/7/15
+ * @time: 1:53 PM
+ */
+public class ConvertAToB {
+
+    public static int getBitsToConvertAToB(int a, int b) {
+        return CountSetBits.countSetBits(a ^ b);
+    }
+
+    public static void main(String a[]) {
+        System.out.println(getBitsToConvertAToB(3, 4));
+        System.out.println(getBitsToConvertAToB(3, 5));
+        System.out.println(getBitsToConvertAToB(5, 3));
+    }
+}
