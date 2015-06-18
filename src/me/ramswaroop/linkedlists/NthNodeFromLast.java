2241912167e819e7165a52fa1dd5d10fb5dd41b4
Ram swaroop <ramswaroop@bsb.in>
2015-06-18 19:15:50 +0530

    nth node from last done

---
+++ src/me/ramswaroop/linkedlists/NthNodeFromLast.java
@@ -0,0 +1,42 @@
+package me.ramswaroop.linkedlists;
+
+import me.ramswaroop.common.SingleLinkedList;
+import me.ramswaroop.common.SingleLinkedNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 6/18/15
+ * @time: 6:49 PM
+ */
+public class NthNodeFromLast<E> extends SingleLinkedList<E> {
+
+    public SingleLinkedNode<E> getNthNodeFromLast(SingleLinkedNode<E> node, int n) {
+        SingleLinkedNode<E> slow = node;
+        SingleLinkedNode<E> fast = node;
+        // move the fast reference ahead of slow reference by 'n' nodes
+        for (int i = 0; i < n; i++) {
+            // assert length of linkedlist > n
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static void main(String a[]) {
+        NthNodeFromLast<Integer> linkedList = new NthNodeFromLast<>();
+        linkedList.add(11);
+        linkedList.add(22);
+        linkedList.add(33);
+        linkedList.add(44);
+        linkedList.add(55);
+        linkedList.add(66);
+        linkedList.add(77);
+        linkedList.add(88);
+        System.out.println(linkedList.getNthNodeFromLast(linkedList.getNode(0), 3).item);
+    }
+}
