c84eb46a096a8b52826c95111d3bb583fa56ed1c
Ram swaroop <ramswaroop@bsb.in>
2015-06-20 12:57:00 +0530

    IsPalindrome done (using stack)

--- src/me/ramswaroop/linkedlists/IsPalindrome.java
+++ src/me/ramswaroop/linkedlists/IsPalindrome.java
@@ -1,7 +1,9 @@
 package me.ramswaroop.linkedlists;
 
+import me.ramswaroop.common.LinkedStack;
 import me.ramswaroop.common.SingleLinkedList;
 import me.ramswaroop.common.SingleLinkedNode;
+import me.ramswaroop.common.Stack;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,18 +15,46 @@ import me.ramswaroop.common.SingleLinkedNode;
 public class IsPalindrome<E> extends SingleLinkedList<E> {
 
     /**
+     * Uses Stack to test whether a linked list starting
+     * from {@param node} is palindrome or not.
+     *
+     * @param node
+     * @return
+     */
+    public static <E> boolean isPalindrome(SingleLinkedNode<E> node) {
+        SingleLinkedNode<E> curr = node;
+        Stack<SingleLinkedNode<E>> stack = new LinkedStack<>();
+
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }
+
+        curr = node;
+
+        while (curr != null) {
+            if (curr.item != stack.pop().item) {
+                return false;
+            }
+            curr = curr.next;
+        }
+
+        return true;
+    }
+
+    /**
      * Recursive function to test whether a linked list
-     * is palindrome or not.
+     * starting from {@param node} is palindrome or not.
      * <p/>
      * NOTE: This method moves the head reference. (disadvantage)
      *
      * @param node
      * @return
      */
-    public boolean isPalindrome(SingleLinkedNode<E> node) {
+    public boolean isPalindromeRecursive(SingleLinkedNode<E> node) {
         if (node == null) return true;
 
-        boolean isPalindrome = isPalindrome(node.next);
+        boolean isPalindrome = isPalindromeRecursive(node.next);
 
         if (head.item == node.item) {
             head = head.next;
@@ -42,6 +72,8 @@ public class IsPalindrome<E> extends SingleLinkedList<E> {
         linkedList.add(11);
         linkedList.add(00);
         linkedList.printList();
-        System.out.println(linkedList.isPalindrome(linkedList.getNode(0)));
+        System.out.println(isPalindrome(linkedList.getNode(0)));
+        linkedList.printList();
+        System.out.println(linkedList.isPalindromeRecursive(linkedList.getNode(0)));
     }
 }
