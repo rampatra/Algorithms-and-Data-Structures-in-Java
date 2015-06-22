3a6a28a1c9d965a1ab12d8b038325db613572753
Ram swaroop <ramswaroop@bsb.in>
2015-06-22 23:39:25 +0530

    removes duplicates from an unsorted linked list

--- src/me/ramswaroop/linkedlists/RemoveDuplicates.java
+++ src/me/ramswaroop/linkedlists/RemoveDuplicates.java
@@ -3,6 +3,9 @@ package me.ramswaroop.linkedlists;
 import me.ramswaroop.common.SingleLinkedList;
 import me.ramswaroop.common.SingleLinkedNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by IntelliJ IDEA.
  *
@@ -13,20 +16,46 @@ import me.ramswaroop.common.SingleLinkedNode;
 public class RemoveDuplicates<E extends Comparable<E>> extends SingleLinkedList<E> {
 
     /**
-     * Removes duplicates in a sorted linked list
+     * Removes duplicates from a sorted linked list
      * by traversing it once.
      *
      * @param list
      * @param <E>
      */
     public static <E extends Comparable<E>> void removeDuplicates(SingleLinkedList<E> list) {
-        SingleLinkedNode<E> firstNode = list.getNode(0), curr = firstNode;
+        SingleLinkedNode<E> curr = list.getNode(0);
         int index = 0;
         while (curr != null) {
+            // inner while loop for removing multiple duplicates
             while (curr.next != null && curr.item == curr.next.item) {
                 list.remove(index + 1);
             }
             index++;
+            curr = curr.next;
+        }
+    }
+
+    /**
+     * Removes duplicates from an unsorted linked list.
+     * <p/>
+     * This method uses {@link java.util.HashMap}, another
+     * way is that you can sort it using merge sort and then
+     * call {@link #removeDuplicates}.
+     *
+     * @param list
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> void removeDuplicatesFromUnsortedList(SingleLinkedList<E> list) {
+        SingleLinkedNode<E> curr = list.getNode(0);
+        Map<E, Boolean> map = new HashMap<>();
+        int index = 0;
+        while (curr != null) {
+            if (map.get(curr.item) == null) {
+                map.put(curr.item, true);
+                index++;
+            } else {
+                list.remove(index);
+            }
             curr = curr.next;
         }
     }
@@ -49,5 +78,19 @@ public class RemoveDuplicates<E extends Comparable<E>> extends SingleLinkedList<
         linkedList.printList();
         removeDuplicates(linkedList);
         linkedList.printList();
+
+        SingleLinkedList<Integer> linkedList2 = new SingleLinkedList<>();
+        linkedList2.add(00);
+        linkedList2.add(00);
+        linkedList2.add(22);
+        linkedList2.add(11);
+        linkedList2.add(44);
+        linkedList2.add(22);
+        linkedList2.add(66);
+        linkedList2.add(77);
+        linkedList2.add(66);
+        linkedList2.printList();
+        removeDuplicatesFromUnsortedList(linkedList2);
+        linkedList2.printList();
     }
 }
