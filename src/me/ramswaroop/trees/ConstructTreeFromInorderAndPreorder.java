711ca8aaf92ca84dff0f614835b175de0b1d7fc4
Ram swaroop <ramswaroop@bsb.in>
2015-06-27 17:34:08 +0530

    pending

--- src/me/ramswaroop/trees/ConstructTreeFromInorderAndPreorder.java
+++ src/me/ramswaroop/trees/ConstructTreeFromInorderAndPreorder.java
@@ -1,5 +1,10 @@
 package me.ramswaroop.trees;
 
+import me.ramswaroop.common.BinaryNode;
+import me.ramswaroop.common.BinaryTree;
+
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  *
@@ -7,10 +12,12 @@ package me.ramswaroop.trees;
  * @date: 6/26/15
  * @time: 5:34 PM
  */
-public class ConstructTreeFromInorderAndPreorder {
+public class ConstructTreeFromInorderAndPreorder<E extends Comparable<E>> extends BinaryTree<E> {
 
-    public void ConstructTreeFromInorderAndPreorder() {
+    public void constructTreeWithInOrderAndPreOrder(List<BinaryNode<E>> inOrder, List<BinaryNode<E>> preOrder) {
+        for (int i = 0; i < preOrder.size(); i++) {
 
+        }
     }
 
     public static void main(String a[]) {
