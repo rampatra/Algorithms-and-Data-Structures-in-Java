4bf77a6e021c94448dc418e81cbb1ca5d94398b0
Ram swaroop <ramswaroop@bsb.in>
2015-07-21 15:23:08 +0530

    power of 2: new method added

--- src/me/ramswaroop/bits/PowerOf2.java
+++ src/me/ramswaroop/bits/PowerOf2.java
@@ -30,17 +30,31 @@ public class PowerOf2 {
         return n != 0 && (n & (n - 1)) == 0; // n != 0 check added for input 0
     }
 
+    /**
+     * The following code can be found in {@link java.util.Random#nextInt(int)}.
+     *
+     * @param n
+     * @return
+     */
+    public static boolean isPowerOf2FromRandomClass(long n) {
+        return n != 0 && (n & -n) == n;
+    }
+
     public static void main(String a[]) {
         System.out.println(isPowerOf2(18));
         System.out.println(isPowerOf2UsingANDoperator(18));
+        System.out.println(isPowerOf2FromRandomClass(18));
 
         System.out.println(isPowerOf2(16));
         System.out.println(isPowerOf2UsingANDoperator(16));
+        System.out.println(isPowerOf2FromRandomClass(16));
 
         System.out.println(isPowerOf2(0)); // works for 0
-        System.out.println(isPowerOf2UsingANDoperator(0)); // works for 0
+        System.out.println(isPowerOf2UsingANDoperator(0)); // works for 0 with a check
+        System.out.println(isPowerOf2FromRandomClass(0)); // works for 0 with a check
 
         System.out.println(isPowerOf2(-2)); // doesn't work for -ve no.s
         System.out.println(isPowerOf2UsingANDoperator(-2)); // doesn't work for -ve no.s
+        System.out.println(isPowerOf2FromRandomClass(-2)); // doesn't work for -ve no.s
     }
 }
