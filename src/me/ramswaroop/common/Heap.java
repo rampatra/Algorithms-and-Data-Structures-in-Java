3ede999e657f20f3ba3c6f9c09d0ae15d53087cf
Ram swaroop <ramswaroop@bsb.in>
2015-08-02 17:45:27 +0530

    heaps : definition/comments added

--- src/me/ramswaroop/common/Heap.java
+++ src/me/ramswaroop/common/Heap.java
@@ -4,6 +4,19 @@ import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
+ * <p/>
+ * A HEAP is a specialized tree-based ABSTRACT DATA TYPE that satisfies the heap property:
+ * min-heap: All non-leaf elements are either smaller than or equal to their left and right child.
+ * max-heap: All non-leaf elements are either greater than or equal to their left and right child.
+ * <p/>
+ * Often implemented as an array, where the children of the element at index i are at index
+ * 2i+1 (left child) and 2i+2 (right child).
+ * <p/>
+ * The first element (minimum or maximum, depending on chosen order) can be found in O(1).
+ * Each successor can be found in O(log n). The algorithm in maxHeapify() takes O(log n) time
+ * Therefore, buildMaxHeap() would take O(n log n) time BUT IF OBSERVED CAREFULLY IT TAKES 0(N) TIME.
+ * <p/>
+ * Used in the HeapSort algorithm. Also can be used to implement a PriorityQueue.
  *
  * @author: ramswaroop
  * @date: 8/2/15
@@ -29,6 +42,11 @@ public class Heap {
         }
     }
 
+    /**
+     * Converts array {@param a} in to a max heap.
+     *
+     * @param a
+     */
     public static void buildMinHeap(int[] a) {
         for (int i = a.length / 2 - 1; i >= 0; i--) {
             minHeapify(a, i);
@@ -53,6 +71,11 @@ public class Heap {
         }
     }
 
+    /**
+     * Converts array {@param a} in to a max heap.
+     *
+     * @param a
+     */
     public static void buildMaxHeap(int[] a) {
         for (int i = a.length / 2 - 1; i >= 0; i--) {
             maxHeapify(a, i);
