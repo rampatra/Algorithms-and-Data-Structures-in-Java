73c82e24851a1052c280b22ce9ddc3a0b7685a32
Ram swaroop <ramswaroop@bsb.in>
2015-08-02 22:49:12 +0530

    kth largest element : done (heap method)

--- src/me/ramswaroop/arrays/KthLargestElement.java
+++ src/me/ramswaroop/arrays/KthLargestElement.java
@@ -1,5 +1,9 @@
 package me.ramswaroop.arrays;
 
+import me.ramswaroop.common.Heap;
+
+import java.util.Arrays;
+
 /**
  * Created by IntelliJ IDEA.
  *
@@ -18,7 +22,7 @@ public class KthLargestElement {
      * @param k
      * @return
      */
-    public static int getKthLargestElement(int[] a, int k) {
+    public static int getKthLargestElementNaive(int[] a, int k) {
         if (k >= a.length) return -1;
 
         a = MergeSort.mergeSort(a);
@@ -26,7 +30,29 @@ public class KthLargestElement {
         return a[a.length - k];
     }
 
+    /**
+     * Determines the kth largest element by building a max heap
+     * k times removing the root each time.
+     *
+     * @param a
+     * @param k
+     * @return
+     */
+    public static int getKthLargestElement(int[] a, int k) {
+        while (true) {
+            Heap.buildMaxHeap(a);
+            if (k == 1) break;
+
+            Heap.swap(a, 0, a.length - 1);
+            a = Arrays.copyOfRange(a, 0, a.length - 1);
+            k--;
+        }
+
+        return a[0];
+    }
+
     public static void main(String a[]) {
+        System.out.println(getKthLargestElementNaive(new int[]{2, 4, 5, 7, 1, 8, 9}, 3));
         System.out.println(getKthLargestElement(new int[]{2, 4, 5, 7, 1, 8, 9}, 3));
     }
 }
