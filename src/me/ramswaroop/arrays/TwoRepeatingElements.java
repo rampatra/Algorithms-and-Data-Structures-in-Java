84cc7a430cc2bfcdffb284ee1600c7b0e1146d57
Ram swaroop <ramswaroop@bsb.in>
2015-08-17 23:57:07 +0530

    two repeating elements : 90% done

--- src/me/ramswaroop/arrays/TwoRepeatingElements.java
+++ src/me/ramswaroop/arrays/TwoRepeatingElements.java
@@ -1,5 +1,7 @@
 package me.ramswaroop.arrays;
 
+import java.util.Arrays;
+
 /**
  * Created by IntelliJ IDEA.
  *
@@ -10,10 +12,37 @@ package me.ramswaroop.arrays;
 public class TwoRepeatingElements {
 
     public static int[] getTwoRepeatingElements(int[] a) {
-        
+        int xor = a[0];
+        int rightMostSetBit;
+        int x = 0, y = 0;
+        for (int i = 1; i < a.length; i++) {
+            xor ^= a[i];
+        }
+        for (int i = 1; i <= a.length - 2; i++) {
+            xor ^= i;
+        }
+        rightMostSetBit = xor & ~(xor - 1);
+
+        for (int i = 0; i < a.length; i++) {
+            if ((a[i] & rightMostSetBit) == 1) {
+                x ^= a[i];
+            } else {
+                y ^= a[i];
+            }
+        }
+
+        for (int i = 1; i <= a.length - 2; i++) {
+            if ((i & rightMostSetBit) == 1) {
+                x ^= i;
+            } else {
+                y ^= i;
+            }
+        }
+
+        return new int[]{x, y};
     }
 
     public static void main(String a[]) {
-
+        System.out.println(Arrays.toString(getTwoRepeatingElements(new int[]{4, 2, 4, 5, 2, 3, 1})));
     }
 }
