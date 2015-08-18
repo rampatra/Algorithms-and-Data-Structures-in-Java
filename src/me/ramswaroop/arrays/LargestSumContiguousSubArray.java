dc1afa51141ec23990fc4484ff5f6e39f2e29bc6
Ram swaroop <ramswaroop@bsb.in>
2015-08-18 23:14:36 +0530

    largest sum contiguous sub array : done

--- src/me/ramswaroop/arrays/LargestSumContiguousSubArray.java
+++ src/me/ramswaroop/arrays/LargestSumContiguousSubArray.java
@@ -9,11 +9,30 @@ package me.ramswaroop.arrays;
  */
 public class LargestSumContiguousSubArray {
 
-    public void largestSumContiguousSubArray(int a[]) {
+    /**
+     * Based on Kadane's Algorithm. 
+     * 
+     * @param a
+     * @return
+     */
+    public static int getLargestSumOfContiguousSubArray(int a[]) {
+        int maxSum = 0, maxSumTillIndex = 0;
 
+        for (int i = 0; i < a.length; i++) {
+            maxSumTillIndex += a[i];
+            if (maxSumTillIndex < 0) {
+                maxSumTillIndex = 0;
+            }
+            if (maxSumTillIndex > maxSum) {
+                maxSum = maxSumTillIndex;
+            }
+        }
+        return maxSum;
     }
 
     public static void main(String a[]) {
-
+        System.out.println(getLargestSumOfContiguousSubArray(new int[]{-2, 1, -3, 4, 5, -1, 4}));
+        System.out.println(getLargestSumOfContiguousSubArray(new int[]{2, -1, -3, 4, -5, 1, 4}));
+        System.out.println(getLargestSumOfContiguousSubArray(new int[]{-2, -1, -3, -4, -5, -1, -4})); // doesn't work
     }
 }
