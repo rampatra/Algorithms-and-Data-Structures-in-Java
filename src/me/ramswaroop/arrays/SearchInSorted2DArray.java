3438b1297a3615d0c7e78e995bc81b4f41e81eff
Ram swaroop <ramswaroop@bsb.in>
2015-08-24 16:30:07 +0530

    search in sorted 2D array : done

--- src/me/ramswaroop/arrays/SearchInSorted2DArray.java
+++ src/me/ramswaroop/arrays/SearchInSorted2DArray.java
@@ -11,20 +11,29 @@ import java.util.Arrays;
  */
 public class SearchInSorted2DArray {
 
+    /**
+     * Searches {@param value} in a square sized 2-D array {@param a} which is sorted
+     * both row wise and column wise.
+     * <p/>
+     * Time complexity: O(n) where n is size of 2-D array.
+     *
+     * @param a
+     * @param i
+     * @param j
+     * @param value
+     * @return
+     */
     public static int[] search(int[][] a, int i, int j, int value) {
-
-        if (i == a.length && j == a[0].length && i == j && a[i][j] != value) {
-            return new int[]{-1, -1};
+        for (int x = 0; x < a.length; x++) {
+            if (a[i][x] == value) {
+                return new int[]{i, x};
+            } else if (a[x][j] == value) {
+                return new int[]{x, j};
+            }
         }
 
-        if (a[i][j] == value) {
-            return new int[]{i, j};
-        } else if (a[i][j] < value && j < a[0].length - 1 && a[i][j + 1] <= value) {
-            return search(a, i, j + 1, value);
-        } else if (a[i][j] < value && i < a.length - 1 && a[i + 1][j] <= value) {
-            return search(a, i + 1, j, value);
-        } else if (a[i][j] > value && i > 0) {
-            return search(a, i - 1, j, value);
+        if (i < a.length - 1) {
+            return search(a, i + 1, j + 1, value);
         } else {
             return new int[]{-1, -1};
         }
@@ -42,10 +51,17 @@ public class SearchInSorted2DArray {
     public static void main(String a[]) {
         int[][] ar = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         print2DMatrix(ar);
-        System.out.println(Arrays.toString(search(ar, 0, 0, 4)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 6)));
         System.out.println(Arrays.toString(search(ar, 0, 0, 1)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 2)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 3)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 4)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 5)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 6)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 7)));
         System.out.println(Arrays.toString(search(ar, 0, 0, 8)));
         System.out.println(Arrays.toString(search(ar, 0, 0, 9)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 10)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 11)));
+        System.out.println(Arrays.toString(search(ar, 0, 0, 12)));
     }
 }
