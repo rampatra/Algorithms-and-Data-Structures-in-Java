cfb991f0d757245953b1659fe738992485871773
Ram swaroop <ramswaroop@bsb.in>
2015-08-25 15:19:43 +0530

    search in sorted 2D array : more optimized version added

--- src/me/ramswaroop/arrays/SearchInSorted2DArray.java
+++ src/me/ramswaroop/arrays/SearchInSorted2DArray.java
@@ -12,10 +12,13 @@ import java.util.Arrays;
 public class SearchInSorted2DArray {
 
     /**
-     * Searches {@param value} in a square sized 2-D array {@param a} which is sorted
+     * Searches {@param value} in a SQUARE sized 2-D array {@param a} which is sorted
      * both row wise and column wise.
      * <p/>
-     * Time complexity: O(n) where n is size of 2-D array.
+     * Time complexity:
+     * T(n) = T(n) + T(n-1) + T(n-2) + .... + T(1)
+     * T(n) = n(n+1)/2
+     * T(n) = O(n^2) where n is size of 2-D array.
      * <p/>
      * Explanation:
      * Linearly searches across rows and columns until the element is found or till the last element. If
@@ -28,8 +31,8 @@ public class SearchInSorted2DArray {
      * @param value
      * @return an array consisting of co-ordinates if {@param value} is found otherwise {@code new int[]{-1, -1}}.
      */
-    public static int[] search(int[][] a, int i, int j, int value) {
-        for (int x = 0; x < a.length && (a[i][x] <= value || a[x][j] <= value); x++) {
+    public static int[] linearSearchNaive(int[][] a, int i, int j, int value) {
+        for (int x = i; x < a.length && (a[i][x] <= value || a[x][j] <= value); x++) {
             if (a[i][x] == value) {
                 return new int[]{i, x};
             } else if (a[x][j] == value) {
@@ -38,10 +41,40 @@ public class SearchInSorted2DArray {
         }
 
         if (i < a.length - 1) {
-            return search(a, i + 1, j + 1, value);
+            return linearSearchNaive(a, i + 1, j + 1, value);
         } else {
             return new int[]{-1, -1};
         }
+    }
+
+    /**
+     * More efficient way to search in a 2-D array sorted both row wise and column wise.
+     * <p/>
+     * Explanation:
+     * We start from top right corner (we can also start from bottom left corner) and move left if current
+     * element is greater than the value to be searched and bottom if current element is
+     * smaller than the value to be searched.
+     * <p/>
+     * Time complexity: O(m+n) where m = no. of rows, n = no. of columns
+     *
+     * @return
+     * @link http://articles.leetcode.com/2010/10/searching-2d-sorted-matrix-part-ii.html
+     */
+    public static int[] linearSearch(int[][] a, int value) {
+        int i = 0, j = a[0].length - 1; // start from top right corner
+
+        while (i < a.length && j >= 0) {
+            if (a[i][j] == value) {
+                return new int[]{i, j};
+            } else if (a[i][j] > value) {
+                j--; // move left
+            } else {
+                i++; // move down
+            }
+        }
+
+        return new int[]{-1, -1};
+
     }
 
     private static void print2DMatrix(int[][] a) {
@@ -56,17 +89,30 @@ public class SearchInSorted2DArray {
     public static void main(String a[]) {
         int[][] ar = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         print2DMatrix(ar);
-        System.out.println(Arrays.toString(search(ar, 0, 0, 1)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 2)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 3)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 4)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 5)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 6)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 7)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 8)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 9)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 10)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 11)));
-        System.out.println(Arrays.toString(search(ar, 0, 0, 12)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 1)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 2)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 3)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 4)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 5)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 6)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 7)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 8)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 9)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 10)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 11)));
+        System.out.println(Arrays.toString(linearSearchNaive(ar, 0, 0, 12)));
+        System.out.println("============");
+        System.out.println(Arrays.toString(linearSearch(ar, 1)));
+        System.out.println(Arrays.toString(linearSearch(ar, 2)));
+        System.out.println(Arrays.toString(linearSearch(ar, 3)));
+        System.out.println(Arrays.toString(linearSearch(ar, 4)));
+        System.out.println(Arrays.toString(linearSearch(ar, 5)));
+        System.out.println(Arrays.toString(linearSearch(ar, 6)));
+        System.out.println(Arrays.toString(linearSearch(ar, 7)));
+        System.out.println(Arrays.toString(linearSearch(ar, 8)));
+        System.out.println(Arrays.toString(linearSearch(ar, 9)));
+        System.out.println(Arrays.toString(linearSearch(ar, 10)));
+        System.out.println(Arrays.toString(linearSearch(ar, 11)));
+        System.out.println(Arrays.toString(linearSearch(ar, 12)));
     }
 }
