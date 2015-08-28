fbcdf23e52e0e93a2f7c37614236c8f53c5acefc
Ram swaroop <ramswaroop@bsb.in>
2015-08-28 14:32:19 +0530

    consecutive elements : comments added

--- src/me/ramswaroop/arrays/ConsecutiveElements.java
+++ src/me/ramswaroop/arrays/ConsecutiveElements.java
@@ -10,7 +10,23 @@ package me.ramswaroop.arrays;
 public class ConsecutiveElements {
 
     /**
-     * 
+     * Given an unsorted array of numbers, write a function that returns true if array consists of consecutive numbers.
+     * <p/>
+     * Examples:
+     * a) If array is {5, 2, 3, 1, 4}, then the function should return true because the array has consecutive numbers
+     * from 1 to 5.
+     * b) If array is {34, 23, 52, 12, 3 }, then the function should return false because the elements are not consecutive.
+     * c) If the array is {7, 6, 5, 5, 3, 4}, then the function should return false because 5 and 5 are not consecutive.
+     * <p/>
+     * ALGORITHM:
+     * The idea is to check for following two conditions. If following two conditions are true, then return true.
+     * 1) max – min + 1 = n where max is the maximum element in array, min is minimum element in array and n is the number
+     * of elements in array.
+     * 2) All elements are distinct.
+     * <p/>
+     * To check if all elements are distinct, we can create a visited[] array of size n. We can map the ith element of input
+     * array arr[] to visited array by using arr[i] – min as index in visited[].
+     *
      * @param a
      * @return
      */
@@ -18,6 +34,7 @@ public class ConsecutiveElements {
         int min = a[0], max = a[0];
         int[] visitedArray = new int[a.length];
 
+        // find min and max element
         for (int i = 1; i < a.length; i++) {
             if (a[i] < min) {
                 min = a[i];
@@ -27,10 +44,12 @@ public class ConsecutiveElements {
             }
         }
 
+        // diff of max and min should be equal to length of array
         if (a.length != max - min + 1) {
             return false;
         }
 
+        // check for distinct elements
         for (int i = 0; i < a.length; i++) {
             if (visitedArray[a[i] - min] == 0) {
                 visitedArray[a[i] - min] = a[i];
