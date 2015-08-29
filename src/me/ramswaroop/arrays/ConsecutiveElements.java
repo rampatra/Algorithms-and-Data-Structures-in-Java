0379082ee99e60bc3cf8a827a9445219c2e16fa6
Ram swaroop <ramswaroop@bsb.in>
2015-08-29 13:04:44 +0530

    consecutive elements : added another method which uses O(1) auxiliary space

--- src/me/ramswaroop/arrays/ConsecutiveElements.java
+++ src/me/ramswaroop/arrays/ConsecutiveElements.java
@@ -25,7 +25,7 @@ public class ConsecutiveElements {
      * 2) All elements are distinct.
      * <p/>
      * To check if all elements are distinct, we can create a visited[] array of size n. We can map the ith element of input
-     * array arr[] to visited array by using arr[i] – min as index in visited[].
+     * array arr[] to visited array by using arr[i] – min as index in visited[]. So we need O(n) auxiliary space.
      *
      * @param a
      * @return
@@ -61,10 +61,53 @@ public class ConsecutiveElements {
         return true;
     }
 
+    /**
+     * This approach is similar to {@link me.ramswaroop.arrays.ConsecutiveElements#areConsecutiveElements(int[])} but
+     * requires O(1) auxiliary space instead of O(n). But the only con of this method is that it modifies the original
+     * input array {@param a}.
+     *
+     * @param a
+     * @return
+     */
+    public static boolean areConsecutiveElementsInO1Space(int[] a) {
+        int min = a[0], max = a[0];
+
+        // find min and max element
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+
+        // diff of max and min should be equal to length of array
+        if (a.length != max - min + 1) {
+            return false;
+        }
+
+        // check for distinct elements
+        for (int i = 0; i < a.length; i++) {
+            if (a[Math.abs(a[i]) - min] >= 0) {
+                a[Math.abs(a[i]) - min] = -(a[Math.abs(a[i]) - min]);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String a[]) {
         System.out.println(areConsecutiveElements(new int[]{5, 4, 3, 2, 1}));
         System.out.println(areConsecutiveElements(new int[]{67, 68, 69, 72, 70, 71}));
         System.out.println(areConsecutiveElements(new int[]{67, 68, 69, 72, 70, 71, 70}));
         System.out.println(areConsecutiveElements(new int[]{8, 5, 2, 4, 3, 1}));
+        System.out.println("==============");
+        System.out.println(areConsecutiveElementsInO1Space(new int[]{5, 4, 3, 2, 1}));
+        System.out.println(areConsecutiveElementsInO1Space(new int[]{67, 68, 69, 72, 70, 71}));
+        System.out.println(areConsecutiveElementsInO1Space(new int[]{67, 68, 69, 72, 70, 71, 70}));
+        System.out.println(areConsecutiveElementsInO1Space(new int[]{8, 5, 2, 4, 3, 1}));
     }
 }
