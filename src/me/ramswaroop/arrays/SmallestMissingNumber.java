59b713155ea73554ee1b8abdf626a70420459a54
Ram swaroop <ramswaroop@bsb.in>
2015-08-30 18:25:23 +0530

    smallest missing number : initial commit

---
+++ src/me/ramswaroop/arrays/SmallestMissingNumber.java
@@ -0,0 +1,19 @@
+package me.ramswaroop.arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 8/29/15
+ * @time: 12:51 PM
+ */
+public class SmallestMissingNumber {
+
+    public void smallestMissingNumber(int[] a) {
+        
+    }
+
+    public static void main(String a[]) {
+
+    }
+}
