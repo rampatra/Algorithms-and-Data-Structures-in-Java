8b7305a1d69d7a4995f00fc6dd4cc128ab1b4365
Ram swaroop <ramswaroop@bsb.in>
2015-08-31 17:01:20 +0530

    majority element in sorted array : refactoring

--- src/me/ramswaroop/arrays/MajorityElementInSortedArray.java
+++ src/me/ramswaroop/arrays/MajorityElementInSortedArray.java
@@ -21,7 +21,7 @@ public class MajorityElementInSortedArray {
      */
     public static boolean isMajorityElement(int[] a, int n) {
         int l = a.length;
-        int startIndex = getIndexOf(a, 0, l - 1, n);
+        int startIndex = getFirstIndexOf(a, n, 0, l - 1);
 
         // element not found
         if (startIndex == -1) return false;
@@ -43,7 +43,7 @@ public class MajorityElementInSortedArray {
      * @param n
      * @return
      */
-    public static int getIndexOf(int[] a, int low, int high, int n) {
+    public static int getFirstIndexOf(int[] a, int n, int low, int high) {
         if (low <= high) {
             int mid = (low + high) / 2;
             /**
@@ -53,12 +53,12 @@ public class MajorityElementInSortedArray {
              * (i)  mid == 0 and a[mid] == n
              * (ii) n > a[mid-1] and a[mid] == n
              */
-            if ((mid == 0 || n > a[mid - 1]) && (a[mid] == n)) {
+            if (a[mid] == n && (mid == 0 || n > a[mid - 1])) {
                 return mid;
             } else if (n <= a[mid]) {
-                return getIndexOf(a, low, mid - 1, n);
+                return getFirstIndexOf(a, n, low, mid - 1);
             } else {
-                return getIndexOf(a, mid + 1, high, n);
+                return getFirstIndexOf(a, n, mid + 1, high);
             }
         }
         return -1;
