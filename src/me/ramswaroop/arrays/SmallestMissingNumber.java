5b80a89c4140775f715769bcb3dba243bad7bc69
Ram swaroop <ramswaroop@bsb.in>
2015-08-31 13:28:15 +0530

    smallest missing number : done

--- src/me/ramswaroop/arrays/SmallestMissingNumber.java
+++ src/me/ramswaroop/arrays/SmallestMissingNumber.java
@@ -9,11 +9,42 @@ package me.ramswaroop.arrays;
  */
 public class SmallestMissingNumber {
 
-    public void smallestMissingNumber(int[] a) {
-        
+    /**
+     * Modified Binary Search to find the smallest missing number in an array
+     * {@param a} consisting of numbers between 0 to m - 1 and m > n where n is
+     * length of array.
+     * <p/>
+     * EXPLANATION:
+     * In standard Binary Search, the element to be searched is compared with
+     * the middle element and on the basis of comparison result, we decide whether
+     * to search is over or to go to left half or right half.
+     * In this method, we modify the standard Binary Search algorithm to compare the 
+     * middle element with its index and make decision on the basis of this comparison.
+     *
+     * @param a
+     * @param low
+     * @param high
+     * @return
+     */
+    public static int smallestMissingNumber(int[] a, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+
+            if (a[mid] == mid) {
+                return smallestMissingNumber(a, mid + 1, high);
+            } else if (a[mid] > mid) {
+                return smallestMissingNumber(a, low, mid - 1);
+            } else {
+                return smallestMissingNumber(a, mid + 1, high);
+            }
+        } else {
+            return low;
+        }
     }
 
     public static void main(String a[]) {
-
+        System.out.println(smallestMissingNumber(new int[]{0, 1, 2, 6, 9}, 0, 4));
+        System.out.println(smallestMissingNumber(new int[]{4, 5, 10, 11}, 0, 3));
+        System.out.println(smallestMissingNumber(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 10}, 0, 8));
     }
 }
