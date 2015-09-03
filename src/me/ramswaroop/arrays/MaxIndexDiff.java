919b8cf615d06e9880d9c1df01c52794c8e0004a
Ram swaroop <ramswaroop@bsb.in>
2015-09-03 10:58:53 +0530

    comments added

--- src/me/ramswaroop/arrays/MaxIndexDiff.java
+++ src/me/ramswaroop/arrays/MaxIndexDiff.java
@@ -10,6 +10,8 @@ package me.ramswaroop.arrays;
 public class MaxIndexDiff {
 
     /**
+     * Given an array arr[], find the maximum j – i such that arr[j] > arr[i].
+     *
      * @param a
      * @return
      */
@@ -17,16 +19,25 @@ public class MaxIndexDiff {
         int maxDiff = -1;
         int[] leftMin = new int[a.length], rightMax = new int[a.length];
 
+        /**
+         * leftMin[i] holds the smallest element on left side of arr[i] including arr[i].
+         * In other words, leftMin[i] stores the minimum value from (arr[0], arr[1], ... arr[i]).
+         */
         leftMin[0] = a[0];
         for (int i = 1; i < a.length; i++) {
             leftMin[i] = Math.min(a[i], leftMin[i - 1]);
         }
 
+        /**
+         * rightMax[i] holds the greatest element on right side of arr[i] including arr[i].
+         * In other words, rightMax[i] stores the maximum value from (arr[i], arr[i+1], ..arr[n-1])
+         */
         rightMax[a.length - 1] = a[a.length - 1];
         for (int i = a.length - 2; i >= 0; i--) {
             rightMax[i] = Math.max(a[i], rightMax[i + 1]);
         }
 
+        // traverse both arrays from left to right to find maximum j - i
         for (int i = 0, j = 0; i < a.length && j < a.length; ) {
             if (rightMax[j] > leftMin[i]) {
                 maxDiff = Math.max(maxDiff, j - i);
