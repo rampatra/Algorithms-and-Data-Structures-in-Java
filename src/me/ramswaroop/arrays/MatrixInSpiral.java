73a1be643d3a9765692ee4b733e55419761d6f35
Ram swaroop <ramswaroop@bsb.in>
2015-09-12 23:04:12 +0530

    code refactoring

--- src/me/ramswaroop/arrays/MatrixInSpiral.java
+++ src/me/ramswaroop/arrays/MatrixInSpiral.java
@@ -55,5 +55,7 @@ public class MatrixInSpiral {
         printMatrixInSpiral(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}});
         out.println();
         printMatrixInSpiral(new int[][]{{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}});
+        out.println();
+        printMatrixInSpiral(new int[][]{{1, 2, 3, 4, 5, 6, 7, 8}, {9, 10, 11, 12, 13, 14, 15, 16}});
     }
 }
--- src/me/ramswaroop/misc/Equals.java
+++ src/me/ramswaroop/misc/Equals.java
@@ -1,4 +1,4 @@
-package me.ramswaroop.practice;
+package me.ramswaroop.misc;
 
 /**
  * Created by IntelliJ IDEA.
