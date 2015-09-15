af590580f864933293000dff8fe6b2af1dba3d37
Ram swaroop <ramswaroop@bsb.in>
2015-09-15 23:34:45 +0530

    initial commit

---
+++ src/me/ramswaroop/misc/Parenthesis.java
@@ -0,0 +1,64 @@
+package me.ramswaroop.misc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 9/15/15
+ * @time: 11:15 PM
+ */
+public class Parenthesis {
+
+    public static String isWellFormed(String input) {
+        int len = input.length() - 1;
+        for (int i = 0; i < len / 2; i++) {
+            if (input.charAt(0) != input.charAt(len - i - 1)) {
+                return "False";
+            }
+        }
+        return "True";
+    }
+
+    public static void areParenthesisWellFormed(String filename) {
+        List<String> input = readFile(filename);
+        for (int i = 0; i < input.size(); i++) {
+            isWellFormed(input.get(i));
+        }
+    }
+
+    public static List<String> readFile(String filename) {
+        List<String> input = new ArrayList<>();
+        BufferedReader br = null;
+
+        try {
+
+            String sCurrentLine;
+
+            br = new BufferedReader(new FileReader(filename));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                input.add(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return input;
+    }
+
+    public static void main(String a[]) {
+        areParenthesisWellFormed(a[0]);
+    }
+}
