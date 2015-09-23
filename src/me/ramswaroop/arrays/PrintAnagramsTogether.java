5e3c4c60400d004a84bd3beb56b6f0a9e4f6d0b9
Ram swaroop <ramswaroop@bsb.in>
2015-09-23 20:48:00 +0530

    coded + unit tested

---
+++ src/me/ramswaroop/arrays/PrintAnagramsTogether.java
@@ -0,0 +1,42 @@
+package me.ramswaroop.arrays;
+
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 9/23/15
+ * @time: 8:11 PM
+ */
+public class PrintAnagramsTogether {
+
+    public static void printAnagramsTogether(String[] s) {
+
+        HashMap<String, List<Integer>> hashMap = new HashMap<>();
+
+        for (int i = 0; i < s.length; i++) {
+            char[] chars = s[i].toCharArray();
+            Arrays.sort(chars);
+            
+            List<Integer> indexes = hashMap.get(String.valueOf(chars));
+            if (indexes == null) {
+                indexes = new ArrayList<>();
+            }
+            indexes.add(i);
+            hashMap.put(String.valueOf(chars), indexes);
+        }
+
+        for (Map.Entry<String, List<Integer>> entry : hashMap.entrySet()) {
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                System.out.println(s[entry.getValue().get(i)]);
+            }
+            System.out.println("------");
+        }
+    }
+
+    public static void main(String a[]) {
+        printAnagramsTogether(new String[]{"cat", "dog", "tac", "god", "act"});
+        printAnagramsTogether(new String[]{"cat", "tac", "act", "god", "dog"});
+    }
+}
