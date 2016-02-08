cdbe9a16835018e009737fdd9a72d05b809989d0
Ram swaroop <ramswaroop@bsb.in>
2016-02-08 11:50:01 +0530

    half done

---
+++ src/com/hackerrank/projecteuler/MultiplesOf3and5.java
@@ -0,0 +1,20 @@
+package com.hackerrank.projecteuler;
+
+import java.util.Scanner;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: ramswaroop
+ * @date: 1/1/16
+ * @time: 8:48 AM
+ */
+public class MultiplesOf3and5 {
+
+    public static void main(String a[]) {
+        Scanner in = new Scanner(System.in);
+        
+        int t = Integer.parseInt(in.nextLine());
+        
+    }
+}
