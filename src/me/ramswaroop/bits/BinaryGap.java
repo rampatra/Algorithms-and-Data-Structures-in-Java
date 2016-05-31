c7292200abca6ffbed9d25b33182dd56b1c882f4
ramswaroop <ramswarooppatra@gmail.com>
2016-05-31 19:00:46 -0400

    fixed bug + count div done

--- src/me/ramswaroop/bits/BinaryGap.java
+++ src/me/ramswaroop/bits/BinaryGap.java
@@ -18,15 +18,14 @@ public class BinaryGap {
         int maxGap = 0;
         while (n > 0) {
             if ((n & 1) == 1) {
-                n = n >>> 1;
-                while (n > 0 && (n & 1) == 0) {
+                while (n >>> 1 > 0 && (n >>> 1 & 1) == 0) {
                     gap++;
                     n = n >>> 1;
                 }
                 if (gap > maxGap) {
                     maxGap = gap;
-                    gap = 0;
                 }
+                gap = 0;
             }
             n = n >>> 1;
         }
@@ -45,5 +44,7 @@ public class BinaryGap {
         System.out.println(findBinaryGap(16));
         System.out.println(findBinaryGap(17));
         System.out.println(findMaxNoOf0sBetweenTwo1s(121));
+        System.out.println(findMaxNoOf0sBetweenTwo1s(1041));
+        System.out.println(findMaxNoOf0sBetweenTwo1s(2_147_483_64889L));
     }
 }
