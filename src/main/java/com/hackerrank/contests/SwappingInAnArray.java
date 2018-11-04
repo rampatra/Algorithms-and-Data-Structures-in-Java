0668e5db044d92a5513f68f91a159b121c9f135e
Ram Patra <ramswarooppatra@gmail.com>
2018-11-04 12:34:52 +0000

    Added main method

--- src/main/java/com/hackerrank/contests/SwappingInAnArray.java
+++ src/main/java/com/hackerrank/contests/SwappingInAnArray.java
@@ -1,5 +1,10 @@
 package com.hackerrank.contests;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author rpatra16
  * @since 04/11/2018
@@ -35,7 +40,31 @@ public class SwappingInAnArray {
         a[j] = temp;
     }
 
-    public static void main(String[] args) {
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int result = swapToSort(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
