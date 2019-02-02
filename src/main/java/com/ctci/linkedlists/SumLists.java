befa3afa4ae14e21f5fb351b300eadd795ad7a7e
Ram Patra <ramswarooppatra@gmail.com>
2019-02-02 15:51:07 +0000

    Added some comments

--- src/main/java/com/ctci/linkedlists/SumLists.java
+++ src/main/java/com/ctci/linkedlists/SumLists.java
@@ -8,6 +8,20 @@ import static com.ctci.linkedlists.Node.printList;
  */
 public class SumLists {
 
+    /**
+     * You have two numbers represented by a linked list, where each node contains a single digit. The digits are
+     * stored in reverse order, such that the 1's digit is at the head of the list (or in other words, the least
+     * significant digit is stored at the head of the list). Write a function that adds the two numbers and returns
+     * the sum as a linked list.
+     * <p>
+     * EXAMPLE
+     * Input: (7-> 1 -> 6) + (5 -> 9 -> 2).That is, 617 + 295.
+     * Output: 2 -> 1 -> 9. That is, 912.
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
     private static Node sumLists(Node num1, Node num2) {
         int carry = 0;
         int sum;
