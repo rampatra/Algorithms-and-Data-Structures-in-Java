932f210c0d21f7842cde857a3e57e9521b2bc0c3
Ram Patra <ramswarooppatra@gmail.com>
2019-03-30 11:14:51 +0000

    Minor refactorings

--- src/main/java/com/rampatra/blockchain/Peer.java
+++ src/main/java/com/rampatra/blockchain/Peer.java
@@ -3,9 +3,7 @@ package com.rampatra.blockchain;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
@@ -59,7 +57,7 @@ public class Peer {
     }
 
     /**
-     * Once a new peer is created and added to the network, it requests the latest block from 
+     * Once a new peer is created and added to the network, it requests the latest block from
      * all the peers and updates its blockchain if it is outdated.
      */
     private void getLatestBlockFromPeers() {
@@ -118,72 +116,5 @@ public class Peer {
         sb.append(", port=").append(port);
         sb.append('}');
         return sb.toString();
-    }
-
-    /**
-     * The main starting point of the blockchain demo. At first, add some peers (option 1) and mine some data 
-     * by choosing a particular peer (option 2). You would soon see that the newly mined block is broadcast to
-     * all the peers.
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        try {
-            int menuChoice;
-            int peerIndex;
-            String data;
-            Scanner s = new Scanner(System.in);
-            Blockchain blockchain = new Blockchain(new ArrayList<>(), 3);
-
-            while (true) {
-
-                System.out.println("\n======= Welcome to Blockchain in Java =======");
-                System.out.println("1. Add Peer");
-                System.out.println("2. Mine data in peer");
-                System.out.println("3. Remove peer");
-                System.out.println("4. Show peers");
-                System.out.println("5. Exit");
-
-                menuChoice = s.nextInt();
-
-                switch (menuChoice) {
-                    case 1:
-                        P2P.addPeer(blockchain);
-                        System.out.println("New peer added!");
-                        P2P.showPeersWithBlockchain();
-                        break;
-                    case 2:
-                        System.out.println("Choose peer: (a number for ex. 1, 2, etc.)");
-                        P2P.showPeers();
-                        peerIndex = s.nextInt();
-                        Peer p = P2P.getPeer(peerIndex - 1);
-                        System.out.println("Enter data: (a string with no spaces)");
-                        data = s.next();
-                        p.mine(data);
-                        System.out.println("Data mined!");
-                        P2P.showPeersWithBlockchain();
-                        break;
-                    case 3:
-                        System.out.println("Choose peer: (a number for ex. 1, 2, etc.)");
-                        P2P.showPeers();
-                        peerIndex = s.nextInt();
-                        P2P.removePeer(peerIndex - 1);
-                        System.out.println("Peer " + peerIndex + " removed!");
-                        P2P.showPeersWithBlockchain();
-                        break;
-                    case 4:
-                        P2P.showPeersWithBlockchain();
-                        break;
-                    case 5:
-                        P2P.removeAllPeers();
-                        System.out.println("Bye, see you soon!");
-                        System.exit(0);
-                    default:
-                        System.out.println("Wrong choice!");
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
