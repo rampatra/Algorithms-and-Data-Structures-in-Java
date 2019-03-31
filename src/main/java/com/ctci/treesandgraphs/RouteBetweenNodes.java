2005be4ae23e15b136599b7ec1898e3e44d2106a
Ram Patra <ramswarooppatra@gmail.com>
2019-03-31 17:50:31 +0100

    BuildOrder: done, time complexity: pending

--- src/main/java/com/ctci/treesandgraphs/RouteBetweenNodes.java
+++ src/main/java/com/ctci/treesandgraphs/RouteBetweenNodes.java
@@ -13,8 +13,73 @@ import java.util.Set;
  */
 public class RouteBetweenNodes {
 
+    class Graph {
+
+        private final Map<Integer, GraphNode> nodes = new HashMap<>();
+
+        /**
+         * Adds an edge from a node with value {@code v1} to another node with value {@code v2}.
+         * Note: This code doesn't work for nodes having duplicate values.
+         *
+         * @param v1
+         * @param v2
+         */
+        void addEdge(int v1, int v2) {
+            GraphNode n1 = nodes.get(v1);
+            GraphNode n2 = nodes.get(v2);
+
+            if (n1 == null) {
+                n1 = new GraphNode(v1);
+                nodes.put(v1, n1);
+            }
+            if (n2 == null) {
+                n2 = new GraphNode(v2);
+                nodes.put(v2, n2);
+            }
+
+            n1.adjacent.add(n2); // as it is a directed graph
+        }
+
+        /**
+         * Checks for a path from a node with value {@code v1} to another node with value {@code v2} in a breadth-first
+         * manner. Note: This code doesn't work for nodes having duplicate values.
+         *
+         * @param v1 the value of the first node or starting node.
+         * @param v2 the value of the ending node.
+         * @return {@code true} if path exists, {@code false} otherwise.
+         */
+        boolean isRoutePresent(int v1, int v2) {
+            Queue<GraphNode> queue = new ArrayDeque<>();
+            Set<GraphNode> visited = new HashSet<>();
+
+            GraphNode n1 = nodes.get(v1);
+            GraphNode n2 = nodes.get(v2);
+
+            if (n1 == null || n2 == null) {
+                return false;
+            }
+
+            queue.add(n1);
+
+            while (!queue.isEmpty()) {
+                GraphNode n = queue.poll();
+
+                if (visited.contains(n)) {
+                    continue;
+                }
+                if (n.adjacent.contains(n2)) {
+                    return true;
+                }
+                queue.addAll(n.adjacent);
+                visited.add(n);
+            }
+
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        Graph g = new Graph();
+        Graph g = new RouteBetweenNodes().new Graph();
         g.addEdge(1, 2);
         g.addEdge(2, 3);
         g.addEdge(4, 5);
@@ -25,70 +90,5 @@ public class RouteBetweenNodes {
         System.out.println("Route exists from 4 to 6: " + g.isRoutePresent(4, 6));
         System.out.println("Route exists from 6 to 4: " + g.isRoutePresent(6, 4));
         System.out.println("Route exists from 6 to 5: " + g.isRoutePresent(6, 5));
-    }
-}
-
-class Graph {
-
-    private static final Map<Integer, GraphNode> nodes = new HashMap<>();
-
-    /**
-     * Adds an edge from a node with value {@code v1} to another node with value {@code v2}.
-     * Note: This code doesn't work for nodes having duplicate values.
-     *
-     * @param v1
-     * @param v2
-     */
-    void addEdge(int v1, int v2) {
-        GraphNode n1 = nodes.get(v1);
-        GraphNode n2 = nodes.get(v2);
-
-        if (n1 == null) {
-            n1 = new GraphNode(v1);
-            nodes.put(v1, n1);
-        }
-        if (n2 == null) {
-            n2 = new GraphNode(v2);
-            nodes.put(v2, n2);
-        }
-
-        n1.adjacent.add(n2); // as it is a directed graph
-    }
-
-    /**
-     * Checks for a path from a node with value {@code v1} to another node with value {@code v2} in a breadth-first
-     * manner. Note: This code doesn't work for nodes having duplicate values.
-     *
-     * @param v1 the value of the first node or starting node.
-     * @param v2 the value of the ending node.
-     * @return {@code true} if path exists, {@code false} otherwise.
-     */
-    boolean isRoutePresent(int v1, int v2) {
-        Queue<GraphNode> queue = new ArrayDeque<>();
-        Set<GraphNode> visited = new HashSet<>();
-
-        GraphNode n1 = nodes.get(v1);
-        GraphNode n2 = nodes.get(v2);
-
-        if (n1 == null || n2 == null) {
-            return false;
-        }
-
-        queue.add(n1);
-
-        while (!queue.isEmpty()) {
-            GraphNode n = queue.poll();
-
-            if (visited.contains(n)) {
-                continue;
-            }
-            if (n.adjacent.contains(n2)) {
-                return true;
-            }
-            queue.addAll(n.adjacent);
-            visited.add(n);
-        }
-
-        return false;
     }
 }
--- src/main/java/com/rampatra/database/README.md
+++ src/main/java/com/rampatra/database/README.md
@@ -1,5 +1,27 @@
 # Relational Database (WIP)
 
+A __relational database__ is a digital database based on
+the [relational model](https://en.wikipedia.org/wiki/Relational_model) of
+data. In simple words, it is a collection of data items with pre-defined 
+relationships between them. These items are organized as a set of tables,
+with columns and rows. Each column stores some specific attribute of an object/entity and the
+row represents a specific object/entity.
+
+A software system used to maintain relational databases is a __relational
+database management system (RDBMS)__, for e.g., MySQL, Oracle DB, PostgreSQL, etc. 
+Virtually all relational database systems use __SQL (Structured Query Language)__ 
+for querying and maintaining the database.
+
+## Basic Definitions
+
+1. Primary Key
+
+2. Candidate Key
+
+3. Composite Key
+
+4. Prime/Non-prime attribute
+
 ## Types of SQL commands
 
 1. DDL
