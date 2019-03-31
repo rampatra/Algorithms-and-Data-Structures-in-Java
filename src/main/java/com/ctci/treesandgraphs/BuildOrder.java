89667bdfadccf1e47b62eef0ce1af55865e361b9
Ram Patra <ramswarooppatra@gmail.com>
2019-03-31 17:54:27 +0100

    Added some comments

--- src/main/java/com/ctci/treesandgraphs/BuildOrder.java
+++ src/main/java/com/ctci/treesandgraphs/BuildOrder.java
@@ -10,6 +10,13 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 /**
+ * You are given a list of projects and a list of dependencies (which is a list of pairs of projects, where the second
+ * project is dependent on the first project). All of a project's dependencies must be built before the project is. Find
+ * a build order that will allow the projects to be built. If there is no valid build order, return an error.
+ * EXAMPLE
+ * Input: projects: a, b, c, d, e, f and dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+ * Output: f, e, a, b, d, c
+ *
  * @author rampatra
  * @since 2019-02-21
  */
@@ -109,7 +116,8 @@ public class BuildOrder {
                ↓           |
                f <–– a <–– d <–– c  
                
-               
+            Note: Project "a" is dependent on "f", and project "d" is dependent on "a", and so on.
+                  
          */
         BuildOrder buildOrder = new BuildOrder();
         buildOrder.addProjects(Stream.of("a", "b", "c", "d", "e", "f"));
