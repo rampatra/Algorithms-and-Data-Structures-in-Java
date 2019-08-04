7d9dec6de6a972ee6bb60b1e379772d167ecb1f0
Ram Patra <ramswarooppatra@gmail.com>
2019-08-04 15:17:51 +0100

    closest bst value: done

--- src/main/java/com/leetcode/trees/ClosestBinarySearchTreeValue.java
+++ src/main/java/com/leetcode/trees/ClosestBinarySearchTreeValue.java
@@ -18,21 +18,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ClosestBinarySearchTreeValue {
 
     /**
-     * @param node
-     * @param parentNode
-     * @param val
-     * @param diff
+     * Runtime: <a href="https://leetcode.com/submissions/detail/248841443/">0 ms</a>.
+     *
+     * @param root
+     * @param target
      * @return
      */
-    public static TreeNode findNodeWithClosestValue(TreeNode node, TreeNode parentNode, int val, int diff) {
-        if (node == null) return parentNode;
+    public static int closestValue(TreeNode root, double target) {
+        if (root == null) return -1;
 
-        if (Math.abs(node.val - val) > diff) return parentNode;
+        return closestValue(root, root, target);
+    }
+
+    private static int closestValue(TreeNode node, TreeNode closestNode, double val) {
+        if (node == null) return closestNode.val;
+
+        if (Math.abs(node.val - val) < Math.abs(closestNode.val - val)) {
+            closestNode = node;
+        }
 
         if (node.val > val) {
-            return findNodeWithClosestValue(node.left, node, val, Math.abs(node.val - val));
+            return closestValue(node.left, closestNode, val);
         } else {
-            return findNodeWithClosestValue(node.right, node, val, Math.abs(node.val - val));
+            return closestValue(node.right, closestNode, val);
         }
     }
 
@@ -54,17 +62,17 @@ public class ClosestBinarySearchTreeValue {
         root.right = new TreeNode(13);
         root.left.left = new TreeNode(5);
         root.left.right = new TreeNode(8);
-        root.left.left.left = new TreeNode(2);
         root.left.left.right = new TreeNode(6);
+        root.left.left.left = new TreeNode(2);
         root.right.right = new TreeNode(20);
 
-        assertEquals(13, findNodeWithClosestValue(root, root, 15, Integer.MAX_VALUE).val);
-        assertEquals(13, findNodeWithClosestValue(root, root, 13, Integer.MAX_VALUE).val);
-        assertEquals(9, findNodeWithClosestValue(root, root, 9, Integer.MAX_VALUE).val);
-        assertEquals(2, findNodeWithClosestValue(root, root, 2, Integer.MAX_VALUE).val);
-        assertEquals(2, findNodeWithClosestValue(root, root, 1, Integer.MAX_VALUE).val);
-        assertEquals(6, findNodeWithClosestValue(root, root, 6, Integer.MAX_VALUE).val);
-        assertEquals(13, findNodeWithClosestValue(root, root, 11, Integer.MAX_VALUE).val); // tie b/w 9 and 13
+        assertEquals(13, closestValue(root, 15));
+        assertEquals(13, closestValue(root, 13));
+        assertEquals(9, closestValue(root, 9));
+        assertEquals(2, closestValue(root, 2));
+        assertEquals(2, closestValue(root, 1));
+        assertEquals(6, closestValue(root, 6));
+        assertEquals(13, closestValue(root, 11)); // tie b/w 9 and 13
 
         /*
             BST looks like:
@@ -83,6 +91,20 @@ public class ClosestBinarySearchTreeValue {
         root.right.left = new TreeNode(13);
         root.right.right = new TreeNode(20);
 
-        assertEquals(13, findNodeWithClosestValue(root, root, 15, Integer.MAX_VALUE).val);
+        assertEquals(13, closestValue(root, 15));
+
+        /*
+            BST looks like:
+
+                    1500000000
+                        /
+                       /
+                      /
+                 1400000000
+         */
+        root = new TreeNode(1500000000);
+        root.left = new TreeNode(1400000000);
+
+        assertEquals(1400000000, closestValue(root, -1500000000.0));
     }
 }
