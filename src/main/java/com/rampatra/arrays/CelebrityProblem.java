334b0377322ee67efbe4a293e2f1a865fe877524
vinhtqfx07044 <vinhtqfx07044@funix.edu.vn>
2023-11-20 11:52:30 +0700

    add junit test and refactor CelebrityProblem.java

--- src/main/java/com/rampatra/arrays/CelebrityProblem.java
+++ src/main/java/com/rampatra/arrays/CelebrityProblem.java
@@ -26,7 +26,9 @@ public class CelebrityProblem {
      * @param b
      * @return
      */
-    public static boolean haveAcquaintance(int[][] peoples, int a, int b) {
+
+    // Rename Method: haveAcquaintance -> doesKnow, for better clarity
+    public static boolean doesKnow(int[][] peoples, int a, int b) {
         return peoples[a][b] == 1;
     }
 
@@ -48,38 +50,47 @@ public class CelebrityProblem {
      * @param peoples
      * @return
      */
+
+    // Extract Method: Extract the celebrity finding logic into a new method
+    private static int findCelebrityInStack(Stack<Integer> possibleCelebrities, int[][] peoples) {
+        int firstPerson = -1, secondPerson;
+
+        // Replace Nested Conditional with Guard Clauses: replace if within while with a guard clause
+        while (true) {
+            firstPerson = possibleCelebrities.pop();
+            // we have found the celebrity
+            if (possibleCelebrities.isEmpty()) {
+                break;
+            }
+            secondPerson = possibleCelebrities.pop();
+            if (doesKnow(peoples, firstPerson, secondPerson)) {
+                possibleCelebrities.push(secondPerson);
+            } else {
+                possibleCelebrities.push(firstPerson);
+            }
+        }
+        return firstPerson;
+    }
+
     public static int findCelebrity(int[][] peoples) {
 
         Stack<Integer> possibleCelebrities = new LinkedStack<>();
 
         for (int i = 0; i < peoples.length; i++) {
             for (int j = 0; j < peoples[0].length; j++) {
-                if (haveAcquaintance(peoples, i, j)) {
+                if (doesKnow(peoples, i, j)) {
                     possibleCelebrities.push(j);
                 }
             }
         }
 
-        int firstPerson = -1, secondPerson;
-        while (!possibleCelebrities.isEmpty()) {
-            firstPerson = possibleCelebrities.pop();
-
-            // we have found the celebrity
-            if (possibleCelebrities.isEmpty()) break;
-
-            secondPerson = possibleCelebrities.pop();
-            if (haveAcquaintance(peoples, firstPerson, secondPerson)) {
-                possibleCelebrities.push(secondPerson);
-            } else {
-                possibleCelebrities.push(firstPerson);
-            }
-        }
-
-        return firstPerson;
+        // Use the newly created findCelebrityInStack method
+        return findCelebrityInStack(possibleCelebrities, peoples);
     }
 
     public static void main(String[] args) {
         System.out.println(findCelebrity(new int[][]{{0, 0, 1, 0}, {0, 0, 1, 0}, {0, 0, 0, 0}, {0, 0, 1, 0}}));
         System.out.println(findCelebrity(new int[][]{{0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}}));
     }
+
 }
