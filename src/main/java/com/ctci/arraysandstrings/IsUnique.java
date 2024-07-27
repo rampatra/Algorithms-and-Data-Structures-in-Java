240acbb59335efce1d766d64b987a35ba2807bb5
Dat Mai <datmv01@gmail.com>
2024-07-27 20:27:50 +0700

    Change method name and enhance code

--- src/main/java/com/ctci/arraysandstrings/IsUnique.java
+++ src/main/java/com/ctci/arraysandstrings/IsUnique.java
@@ -6,16 +6,33 @@ package com.ctci.arraysandstrings;
  */
 public class IsUnique {
 
-    private static boolean hasAllUniqueCharacters(String str) {
-        if (str == null || str.length() > 128) return false;
+    /**
+     * Check whether the input string contains different individual characters and it in the ASCII table.
+     *
+     * @param str Input string
+     * @return true if all characters are different from each other, otherwise false.
+     */
+    public static boolean isAllCharactersUniqueAndInASCII(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
 
-        boolean[] charSet = new boolean[128]; // assuming the string contains only ASCII characters
-        for (int i = 0; i < str.length(); i++) {
-            int charVal = str.charAt(i);
-            if (charSet[charVal]) {
+        int maxCharIndex = 128;
+        int stringLength = str.length();
+
+        if (stringLength > maxCharIndex) {
+            return false;
+        }
+
+        boolean[] characterTrack = new boolean[maxCharIndex]; // assuming the string contains only ASCII characters
+        for (int i = 0; i < stringLength; i++) {
+            int charIndex = str.charAt(i);
+            if (charIndex >= maxCharIndex
+                    || characterTrack[charIndex]) {
                 return false;
             }
-            charSet[charVal] = true;
+
+            characterTrack[charIndex] = true;
         }
         return true;
     }
@@ -34,11 +51,11 @@ public class IsUnique {
 
     public static void main(String[] args) {
         String s = "ram";
-        System.out.println(hasAllUniqueCharacters(s));
+        System.out.println(isAllCharactersUniqueAndInASCII(s));
         s = "rama";
-        System.out.println(hasAllUniqueCharacters(s));
+        System.out.println(isAllCharactersUniqueAndInASCII(s));
         s = "ramA";
-        System.out.println(hasAllUniqueCharacters(s));
+        System.out.println(isAllCharactersUniqueAndInASCII(s));
         System.out.println("-------");
         s = "ram";
         System.out.println(hasAllUniqueCharactersWhenStringContainsAllLowercase(s));
